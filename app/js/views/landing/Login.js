e0856269db20ac0dcc119bdb1529a9e394e2dbdd
Carlos Vializ <cvializ@vt.edu>
2014-07-23 11:04:08 -0700

    Select the first game in the login list.

--- app/js/views/landing/Login.js
+++ app/js/views/landing/Login.js
@@ -4,8 +4,12 @@ define(['marionette', 'Vent', 'views/landing/Game', 'hbs!templates/landing/login
     template: loginTemplate,
     childView: GameView,
     childViewContainer: '.c-login-games',
-    onRender: function onRender() {
-      $('input[type="radio"]').first().attr('checked', true);
+    onRenderCollection: function collectionRendered() {
+      // The render:collection event is triggered before
+      // the newly rendered element in this.elBuffer is
+      // attached to the DOM. So we need to manipulate this.elBuffer
+      // GROSS
+      this.elBuffer.querySelector('input[type="radio"]').setAttribute('checked', 'checked');
     },
     ui: {
       username: '#c-login-form-username',

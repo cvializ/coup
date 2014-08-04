6c20e69d2d83e72a632958c97cf248a4d56cd724
Carlos Vializ <cvializ@vt.edu>
2014-08-04 16:31:59 -0700

    Add all influence actions to PrimaryAction view.

--- app/js/models/action/influence/cards/index.js
+++ app/js/models/action/influence/cards/index.js
@@ -2,6 +2,6 @@ exports.Default = require('./Default.json');
 
 exports.Ambassador = require('./Ambassador.json');
 exports.Assassin = require('./Assassin.json');
-exports.Duke = require('./Duke.json');
 exports.Captain = require('./Captain.json');
 exports.Contessa = require('./Contessa.json');
+exports.Duke = require('./Duke.json');

78df7ea2347f6cf29d917a7033a7c32cc2e53b0c
Carlos Vializ <cvializ@vt.edu>
2014-08-13 11:45:26 -0700

    Don't create a GameDataControlelr per client, only create one.

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ var
     // Controllers
     LandingController = require('./controllers/Landing'),
     PlayController = require('./controllers/Play'),
-    GameDataController = require('./controllers/GameData');
+    GameDataController = require('./controllers/GameData'),
+
+    // instantiate this controller only once, since it listens for
+    // server-side events only and thus only needs to be created once.
+    gdc = new GameDataController({ emitter: emitter });
 
 io.on('connection', function (socket) {
   socket.join('landing');
 
+  // Create one each of these controllers, since they handle
+  // each client's socket events.
   var lc = new LandingController({ emitter: socket }),
-      pc = new PlayController({ emitter: socket }),
-      gdc = new GameDataController({ emitter: emitter });
+      pc = new PlayController({ emitter: socket });
 
   socket.on('error', function (err) {
     console.log('ERROR!');

5768e2bc5ed111d5c08e136fb4a517c0b10aaf8b
Carlos Vializ <cvializ@vt.edu>
2014-08-19 15:04:25 -0700

    Add more Player tests.

--- tests/models/Player.js
+++ tests/models/Player.js
@@ -53,7 +53,81 @@ describe('Player', function () {
       player.chooseEliminatedCard(function (err) {
         expect(err).to.equal(undefined);
         expect(influences[0].eliminated).to.equal(true);
-      })
+      });
+    });
+  });
+
+  describe('#hasInfluence', function () {
+    it('should return true if the player has an influence and it is not eliminated', function () {
+      expect(player.hasInfluence('Contessa')).to.equal(true);
+    });
+
+    it('should return false if the player does not possess an influence card', function () {
+      expect(player.hasInfluence('Assassin')).to.equal(false);
+    });
+
+    it('should return false if the player possesses the card, but it is eliminated', function () {
+      var ambassador = influences.filter(function (card) { return card.name === 'Ambassador'; }).pop();
+
+      expect(player.hasInfluence(ambassador.name)).to.equal(true);
+      ambassador.eliminated = true;
+      expect(player.hasInfluence(ambassador.name)).to.equal(false);
+    });
+  });
+
+  describe('#eliminateCard', function () {
+    it('should set the card with the given id to eliminated', function () {
+      var ambassador = influences.filter(function (card) { return card.name === 'Ambassador'; }).pop();
+
+      expect(ambassador.eliminated).to.equal(false);
+      player.eliminateCard(ambassador.id);
+      expect(ambassador.eliminated).to.equal(true);
+    });
+
+    it('should eliminate the player when they have eliminated all influence', function () {
+      var ambassador = influences.filter(function (card) { return card.name === 'Ambassador'; }).pop(),
+          contessa = influences.filter(function (card) { return card.name === 'Contessa'; }).pop();
+
+      expect(player.eliminated).to.equal(false);
+
+      player.eliminateCard(ambassador.id);
+      player.eliminateCard(contessa.id);
+
+      expect(player.eliminated).to.equal(true);
+    });
+  });
+
+  describe('#getClientObject', function () {
+    it('should not expose uneliminated cards to other users', function () {
+      var clientObject = player.getClientObject({ privileged: false }),
+          key;
+
+      for (key in clientObject.influences) {
+        expect(clientObject.influences[key].dummy).to.equal(true);
+      }
+    });
+
+    it('should expose eliminated cards to other users', function () {
+      var clientObject,
+          key,
+          i;
+      for (i = 0; i < influences.length; i++) {
+        influences[i].eliminated = true;
+      }
+
+      clientObject = player.getClientObject({ privileged: false });
+      for (key in clientObject.influences) {
+        expect(clientObject.influences[key].dummy).to.equal(false);
+      }
+    });
+
+    it('should expose all cards to the player who owns them', function () {
+      var clientObject = player.getClientObject({ privileged: true }),
+          key;
+
+      for (key in clientObject.influences) {
+        expect(clientObject.influences[key].dummy).to.equal(false);
+      }
     });
   });
 

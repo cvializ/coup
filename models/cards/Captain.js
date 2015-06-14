6ae84dd4bbc273d6e8ff6bebc9ba1acc30ea424a
Carlos Vializ <cvializ@vt.edu>
2015-06-14 09:51:34 -0700

    Refactor ternary into Math.min

--- models/cards/Captain.js
+++ models/cards/Captain.js
@@ -1,10 +1,7 @@
 var CaptainAbilities = {
   Steal: function (move, game, callback) {
-    var target = move.target,
-        coinsToSteal = 2;
-
     // Only steal 2 or fewer coins. Don't take more than the player has.
-    coinsToSteal = (target.coins >= coinsToSteal ? coinsToSteal : target.coins );
+    var coinsToSteal = Math.min(move.target.coins, 2);
 
     move.target.coins -= coinsToSteal;
     move.player.coins += coinsToSteal;
